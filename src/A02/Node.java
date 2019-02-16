1f75c08a743e1b6cc473803083ba2fb2255709f3
bjrohbock <bjrohbock@gmail.com>
2019-02-16 10:04:46 -0700

    creates Deque class and Node class

---
+++ src/A02/Node.java
@@ -0,0 +1,52 @@
+package com.company;
+
+class Node<E>
+{
+    E item, next, previous;
+
+
+    public Node(E item, E next, E previous)
+    {
+        this.item = item;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public Node(E item, E next)
+    {
+        this.item = item;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public E getItem()
+    {
+        return this.item;
+    }
+
+    public void setItem(E item)
+    {
+        this.item = item;
+    }
+
+    public E getNext()
+    {
+        return this.next;
+    }
+
+    public void setNext(E next)
+    {
+        this.next = next;
+    }
+
+    public E getPrevious()
+    {
+        return this.previous;
+    }
+
+    public void setPrevious(E previous)
+    {
+        this.previous = previous;
+    }
+
+}
