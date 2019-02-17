e14ce6083365105d040a158f2605ce6a2a8b6b43
Cody Henry <chenry16@bruinmail.slcc.edu>
2019-02-16 21:57:12 -0700

    Add files via upload

---
+++ RandomizedQueue.java
@@ -0,0 +1,145 @@
+package A02;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.lang.NullPointerException;
+import edu.princeton.cs.algs4.StdRandom; 
+
+/**
+ *
+ * @author Isaac Perry
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+	//from algs 4 package
+	 private Item[] collection; 
+	 private int n; 
+	
+	
+    /**
+     * construct an empty randomized queue
+     */
+    public RandomizedQueue(){
+    	
+    	//Code from Assignment 2 Checklist 
+    	collection = (Item[]) new Object[1]; 
+    	n = 0; 
+    	
+    }
+    /**
+     * 
+     * @return is the queue empty?
+     */
+    public boolean isEmpty(){
+    	
+    	return n<=0; 
+    	
+    }
+    /**
+     * 
+     * @return return the number of items on the queue
+     */
+    public int size(){
+    	return n; 
+        
+    }
+    /**
+     * 
+     * @param item add the item
+     */
+    
+    public void enqueue(Item item){
+    	if(item == null) { throw new NullPointerException("That isn't valid data to input"); }
+    	
+    	//resize to double of the current size of array if collection is full. 
+    	if(n == collection.length) { 	int capacity = collection.length * 2; 
+		// textbook implementation
+		Item[] temp = (Item[]) new Object[capacity];
+			for (int i = 0; i < n; i++) { temp[i] =  collection[i];}  collection = temp;
+		       // alternative implementation // a = java.util.Arrays.copyOf(a, capacity);  
+			}
+    	
+    	collection[n++] = item; 
+    	
+    }
+    /**
+     * 
+     * @return delete and return a random item
+     */
+    public Item dequeue(Item item){
+    	if (isEmpty()) throw new NoSuchElementException("Empty Collection");
+        int indx = StdRandom.uniform(n);
+        Item removed = collection[indx];
+       
+        
+        if (indx != n - 1) collection[n] = collection[indx - 1];
+        
+        collection[--n] = null;
+        
+        
+        if (n >= 1 && n == collection.length / 4) { 
+        		int capacity = collection.length / 2; 
+        		// textbook implementation
+        		Item[] temp = (Item[]) new Object[capacity];
+        			for (int i = 0; i < n; i++) { temp[i] =  collection[i];}  collection = temp;
+        		       // alternative implementation // a = java.util.Arrays.copyOf(a, capacity); 
+      } 
+        return removed;
+    }
+    /**
+     * 
+     * @return return (but do not delete) a random item
+     */
+    public Item sample(Item item){
+    	 if (isEmpty()) throw new NoSuchElementException("Empty Collection");
+         return(collection[StdRandom.uniform(n)]); 
+    }
+    /**
+     * 
+     * @return return an independent iterator over items in random order
+     */
+    
+    public Iterator<Item> iterator()
+    {
+       return new RandomizedQueueIterator();
+    }
+    
+    /**
+     * A copy of the original resizing array with the ability to dequeue only.
+     */
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        // Store Items in a temp
+        private int n2 = n;
+        private Item[] temp;
+        
+
+        // an iterator, doesn't implement remove() since it's optional
+        private RandomizedQueueIterator() {
+            temp = (Item[]) new Object[n2];
+            for (int i = 0; i < n2; i++) temp[i] = collection[i];
+        }
+        
+      
+        public boolean hasNext() { return n2 > 0; }
+        
+    
+        public void remove() { 
+            throw new java.lang.UnsupportedOperationException();
+        }
+        
+        public Item next() {
+            if (!hasNext()) throw new java.util.NoSuchElementException();
+            int indx = StdRandom.uniform(n2);
+            Item ans = temp[indx];
+            if (indx != n2 - 1) temp[indx] = temp[n2 - 1];
+            temp[--n2] = null;
+            return ans;
+        }
+    }
+
+
+    
+    public static void main(String[] args){
+        
+    }
+
+}
